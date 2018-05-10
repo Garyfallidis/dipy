08147c7277632ddfd4eee764939743e34ee67952
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2018-05-10 16:14:58 -0400

    Added simple replacement shader

--- doc/examples/viz_surfaces.py
+++ doc/examples/viz_surfaces.py
@@ -27,6 +27,9 @@ from dipy.viz import window
 from dipy.utils.optpkg import optional_package
 vtk, have_vtk, setup_module = optional_package('vtk')
 
+interactive = True
+thick_edges = False
+
 """
 Create an empty ``vtkPolyData``
 """
@@ -99,15 +102,98 @@ Visualize surfaces
 # get vtkActor
 cube_actor = ut_vtk.get_actor_from_polydata(cube_polydata)
 
+
+if thick_edges:
+    cube_actor.GetProperty().SetEdgeVisibility(1)
+    cube_actor.GetProperty().SetEdgeColor(0.9, 0.9, 0.4)
+    cube_actor.GetProperty().SetLineWidth(6)
+    cube_actor.GetProperty().SetPointSize(12)
+    cube_actor.GetProperty().SetRenderLinesAsTubes(1)
+    cube_actor.GetProperty().SetRenderPointsAsSpheres(1)
+    cube_actor.GetProperty().SetVertexVisibility(1)
+    cube_actor.GetProperty().SetVertexColor(0.5, 1.0, 0.8)
+
+
+gl_mapper = cube_actor.GetMapper()
+
+gl_mapper.AddShaderReplacement(
+    vtk.vtkShader.Vertex,
+    "//VTK::ValuePass::Impl",  # replace the normal block
+    False,
+    "//VTK::ValuePass::Impl\n" # we still want the default
+    #"gl_Position = MCDCMatrix * vertexMC;\n"
+    "gl_Position = gl_Position + 10 * sin(vertexMC);\n",
+    #"gl_Position = vec4(0, 0, 0, 1.0);\n",
+    False)
+
+gl_mapper.AddShaderReplacement(
+    vtk.vtkShader.Fragment,
+    "//VTK::Light::Impl",
+    True,
+    "//VTK::Light::Impl\n"
+    "fragOutput0 = vec4(1, 0, 0, lineWidthPercentageBlack);\n abcd",
+    False)
+
+
+gl_mapper.AddShaderReplacement(
+        vtk.vtkShader.Fragment,  # in the fragment shader
+        "//VTK::Coincident::Dec",  # replace the normal block
+        True,  # before the standard replacements
+        "//VTK::Coincident::Dec\n"  # we still want the default
+        # "  uniform vec3 mycolor;\n"
+        # "  uniform vec3 cameraPos;\n"
+        "uniform float lineWidthPercentageBlack;\n"
+        "uniform float lineWidthDepthCueingFactor;\n"
+        "uniform float lineHaloMaxDepth;\n"
+        "uniform vec3 colorLine;\n"
+        "uniform vec3 colorHalo;\n"
+        "in vec3 positionVSOutput;\n"
+        "in vec3 directionVSOutput;\n"
+        "in vec2 uvVSOutput;\n",  # but we add this
+        False)
+
+
 # renderer and scene
 renderer = window.Renderer()
 renderer.add(cube_actor)
 renderer.set_camera(position=(10, 5, 7), focal_point=(0.5, 0.5, 0.5))
+
+camera = renderer.get_camera()
+
+@vtk.calldata_type(vtk.VTK_OBJECT)
+def vtkShaderCallback(caller, event, calldata=None):
+    camera = renderer.GetActiveCamera()
+    cameraPos = camera.GetPosition()
+    projMat = camera.GetProjectionTransformMatrix(renderer)
+    viewMat = camera.GetViewTransformMatrix()
+    program = calldata
+    if program is not None:
+        program.SetUniformf("lineTriangleStripWidth", 0.25)
+        program.SetUniformf("lineWidthPercentageBlack", 0.5)
+        program.SetUniformf("lineWidthDepthCueingFactor", 0.5)
+        program.SetUniformf("lineHaloMaxDepth", 0.005)
+        # program.SetUniformf("lineTriangleStripWidth", 0.05)
+        # program.SetUniformf("lineWidthPercentageBlack", 0.3)
+        # program.SetUniformf("lineWidthDepthCueingFactor", 1.0)
+        # program.SetUniformf("lineHaloMaxDepth", 0.02)
+        program.SetUniform3f("colorLine", [0.0, 0.0, 0.0])
+        program.SetUniform3f("colorHalo", [1.0, 1.0, 1.0])
+        # program.SetUniform3f("colorHalo", [0.8, 0.8, 0.8])
+        program.SetUniform3f("mycolor", [0.4, 0.7, 0.6])
+        program.SetUniform3f("cameraPos", cameraPos)
+        program.SetUniformMatrix("projMat", projMat)
+        program.SetUniformMatrix("viewMat", viewMat)
+
+gl_mapper.AddObserver(vtk.vtkCommand.UpdateShaderEvent,
+                      vtkShaderCallback)
+
+
 renderer.zoom(3)
 
 # display
-# window.show(renderer, size=(600, 600), reset_camera=False)
-window.record(renderer, out_path='cube.png', size=(600, 600))
+if interactive:
+    window.show(renderer, size=(600, 600), reset_camera=False)
+# window.record(renderer, out_path='cube.png', size=(600, 600))
 
 """
 .. figure:: cube.png
