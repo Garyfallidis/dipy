d79f5cca1e3c4ff5b4316861abe32e6f6ec5f49d
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2018-11-19 04:59:32 -0500

    Debugging expected output

--- dipy/align/tests/test_blas.py
+++ dipy/align/tests/test_blas.py
@@ -6,24 +6,32 @@ from dipy.align.bundlemin import (_bundle_minimum_distance,
 np.random.seed(1234)
 rows = 1000
 num_s = 1
+num_threads = 1
 A = np.ones((rows, 3))
 B = np.zeros((rows, 3))
+
+#A = np.random.rand(rows, 3)
+#B = np.random.rand(rows, 3)
+
 
 from time import time
 
 t1 = time()
 
 for i in range(100000):
-    res = _bundle_minimum_distance(A, B, num_s, num_s, rows)
-
+    res = _bundle_minimum_distance(A, B, num_s, num_s,
+                                   rows, num_threads=1)
+    print(res)
 t2 = time()
 
+1/0
 print(res, t2-t1)
 
 t3 = time()
 
 for i in range(100000):
-    res2 = _bundle_minimum_distance_blas(A, B, num_s, num_s, rows)
+    res2 = _bundle_minimum_distance_blas(A, B, num_s, num_s,
+                                         rows, num_threads=1)
 
 t4 = time()
-print(res2, t2-t1)
+print(res2, t4-t3)
